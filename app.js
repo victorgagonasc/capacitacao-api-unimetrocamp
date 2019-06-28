84ba7b7a17aa3d190020ab4e0f015572b29717ab
Victor Gago do Nascimento <victor.nascimento@venturus.org.br>
2019-06-28 20:49:21 -0300

    termino api

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/capacitacao-api-unimetrocamp', {
+    useNewUrlParser: true
+});
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/', require('./routes'));
 
--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,20 +1,23 @@
-const get = function(req, res) {
-    const tasks = [
-        {
-            "title": "Aprender HTML",
-            "description": "Estudar a tag body"
-        },
-        {
-            "title": "Aprender CSS",
-            "description": "Estudar alinhamento"
-        }
-    ];
+const Task = require('../models/Task');
 
-    res.send(tasks);
+const get = async function(req, res) {
+    try {
+        const tasks = await Task.find();  
+
+        return res.send(tasks);    
+    } catch (error) {
+        return res.status(500).send('Erro ao buscar tarefas');
+    }
 }
 
-const create = function(req, res) {
+const create = async function(req, res) {
+    try {
+        const task = await Task.create(req.body);
 
+        return res.send(task);
+    } catch (error) {
+        return res.status(500).send('Erro ao cadastrar tarefa');
+    }
 }
 
 const update = function(req, res) {
